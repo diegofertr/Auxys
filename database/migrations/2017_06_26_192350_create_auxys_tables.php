b68de50ca5620c9ff4868379d3589f3104df3b1b
diegofertr <diegonano09@gmail.com>
2017-06-26 15:43:57 -0400

    Configurando la base de datos

--- database/migrations/2017_06_26_192350_create_auxys_tables.php
+++ database/migrations/2017_06_26_192350_create_auxys_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStudentsTable extends Migration
+class CreateAuxysTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,7 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('nombre');
-            $table->string('ci');
-            $table->string('matricula');
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -29,6 +23,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        //
     }
 }
