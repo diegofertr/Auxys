712ab03b16da93c8f18c1ddbee88f0807433fb95
cechus <cechuscsc4@gmail.com>
2017-07-05 01:55:00 +0000

    bug fixes on routes

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -10,7 +10,7 @@
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
             <li><a href="/convocatoria"><i class='fa fa-link'></i> <span>Convocatorias</span></a></li>
             <li><a href="/materias"><i class='fa fa-link'></i> <span>Materias</span></a></li>
-            <li><a href="{{url('import')}}"><i class='glyphicon glyphicon-import '></i> <span>Importar estudiantes</span></a></li>
+            <li><a href="{{url('student/import')}}"><i class='glyphicon glyphicon-import '></i> <span>Importar estudiantes</span></a></li>
             <li class="treeview">
                 <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
